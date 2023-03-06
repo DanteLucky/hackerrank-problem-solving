b05ff09f6a623a3d5c71169233289cbfbb9a42d3
Lucky-Dante <robinharman61@gmail.com>
2023-03-06 21:36:48 +0000

    Resolve "Subarray Division"

---
+++ algorithm/subarray-division.js
@@ -0,0 +1,26 @@
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(s, d, m) {
+    // Time complexity: O(|s|)
+    if (s.length < m) {
+        return 0;
+    }
+    let sumSquare = s.slice(0, m).reduce((prev, cur) => prev + cur);
+    let result = sumSquare === d ? 1 : 0;
+
+    for (let i = m; i < s.length; i++) {
+        sumSquare = sumSquare - s[i - m] + s[i];
+        if (sumSquare === d) {
+            result++;
+        }
+    }
+    return result
+}
