3c018877d0559ca9514fe20f7706fa5d4ef2d6ae
Lucky-Dante <robinharman61@gmail.com>
2023-03-12 17:35:15 +0000

    Resolve "Electronics Shop"

---
+++ algorithm/electronics-shop.js
@@ -0,0 +1,19 @@
+function getMoneySpent(keyboards, drives, b) {
+    /*
+     * Write your code here.
+     */
+    // Time Complexity: O(m * n)
+    let maxPrice = -1;
+  
+    for (const keyboard of keyboards) {
+      for (const drive of drives) {
+        const curPrice = keyboard + drive;
+        if (curPrice > maxPrice && curPrice <= b) {
+          maxPrice = curPrice;
+        }
+      }
+    }
+  
+    return maxPrice;
+  }
+  
